d552d47d57453d2da8f20fb77093981e9eb27ab1
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-25 12:10:18 +0200

    chore: Update database connection configuration
    This commit updates the `dbConnection.js` file to improve the database connection configuration. It adds a new function `dbConnection` that connects to the MongoDB database using the provided environment variable `MONGODB`. The connection options `useNewUrlParser` and `useUnifiedTopology` are set to `true` for better compatibility. This change enhances the database connection setup for the ISTOCK API.
    
    Note: Please remove any meta information like issue references, tags, or author names from the commit message.

--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -1,5 +1,27 @@
-'use strict'
+"use strict";
 
 /* ---------------------------------- */
 /*             ISTOCK API             */
+/*         Database Connection        */
 /* ---------------------------------- */
+
+// MongoDB Connection:
+
+const mongoose = require("mongoose");
+
+const dbConnection = function () {
+  // Connect:
+  mongoose
+    .connect(process.env.MONGODB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("* DB Connected * "))
+    .catch((err) => console.log("* DB Not Connected * ", err));
+};
+
+/* --------------------------------------------- */
+module.exports = {
+  mongoose,
+  dbConnection,
+};
