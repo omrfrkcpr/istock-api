c0dd4a2e34ae7e0fc509dc1064f1336bec6b58e0
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-25 23:11:27 +0200

    chore: Update token controller to include list, create, read, update, and delete functionality
    This commit updates the `token.js` controller in the `src/controllers` directory to include list, create, read, update, and delete functionality. The `list` function retrieves a list of tokens from the database. The `create` function creates a new token. The `read` function retrieves a specific token by its ID. The `update` function updates a token by its ID. The `delete` function deletes a token by its ID.
    
    Note: Please remove any meta information like issue references, tags, or author names from the commit message.

--- src/controllers/token.js
+++ src/controllers/token.js
@@ -4,3 +4,81 @@
 /*             ISTOCK API             */
 /*          Token Controller          */
 /* ---------------------------------- */
+
+const Token = require("../models/token");
+
+module.exports = {
+  list: async (req, res) => {
+    /*
+            #swagger.ignore = true
+        */
+
+    const data = await res.getModelList(Token);
+
+    res.status(200).send({
+      error: false,
+      details: await res.getModelListDetails(Token),
+      data,
+    });
+  },
+
+  create: async (req, res) => {
+    /*
+            #swagger.ignore = true
+        */
+
+    const data = await Token.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      message: "New Token successfully created!",
+      data,
+    });
+  },
+
+  read: async (req, res) => {
+    /*
+            #swagger.ignore = true
+        */
+
+    const data = await Token.findOne({ _id: req.params.id });
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+
+  update: async (req, res) => {
+    /*
+            #swagger.ignore = true
+        */
+
+    const data = await Token.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
+
+    res.status(202).send({
+      error: false,
+      message: "Token successfully updated!",
+      data,
+      new: await Token.findOne({ _id: req.params.id }),
+    });
+  },
+
+  delete: async (req, res) => {
+    /*
+            #swagger.ignore = true
+        */
+
+    const data = await Token.deleteOne({ _id: req.params.id });
+
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      message: data.deletedCount
+        ? "Token successfully deleted!"
+        : "Token not found!",
+      data,
+    });
+  },
+};
